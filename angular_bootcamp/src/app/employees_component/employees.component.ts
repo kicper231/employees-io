212b873cbea0f54c01d5b815238e5be330b58e99
Kacper <kacper.krol@itsquad.pl>
2024-07-18 15:29:16 +0200

    feat: add employee select logic with old syntax

--- angular_bootcamp/src/app/employees_component/employees.component.ts
+++ angular_bootcamp/src/app/employees_component/employees.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
 import { Employee } from '../models/employee';
 import { FormsModule } from '@angular/forms';
-import { UpperCasePipe } from '@angular/common';
+import { NgForOf, NgIf, UpperCasePipe } from '@angular/common';
+import { EMPLOYESS } from '../mock_data/mock-employees';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [FormsModule, UpperCasePipe],
+  imports: [FormsModule, UpperCasePipe, NgForOf, NgIf],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
 export class EmployeesComponent {
-  exampleEmployee: Employee = {
-    name: 'Kapiszon',
-    hireDate: new Date(Date.now()),
-    id: crypto.randomUUID(),
-    projectsList: [],
-    skillsList: [],
-    surname: 'Kowalski',
-    manager: null,
-  };
+  selectedEmployee?: Employee;
+  listOfEmployees: Employee[] = EMPLOYESS;
+
+  onSelect(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
 }
