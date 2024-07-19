725d1d527721c2b2f21877aa1a92ead2b2866a0a
Kacper <kacper.krol@itsquad.pl>
2024-07-19 15:29:04 +0200

    refactor: add color scheme

--- angular_bootcamp/src/app/employees/employees-list/employees-list.component.ts
+++ angular_bootcamp/src/app/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { EMPLOYESS } from '../../mocks/mock-employees';
 import { Employee } from '../../models/employee.model';
 
 @Component({
