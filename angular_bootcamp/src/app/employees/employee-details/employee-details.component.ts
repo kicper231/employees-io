e34f4c1e0cd2884a5d22dfd5b86efb7cdaf4bdbc
Kacper <kacper.krol@itsquad.pl>
2024-07-23 12:07:56 +0200

    Feat: fix design and add initial form

--- angular_bootcamp/src/app/employees/employee-details/employee-details.component.ts
+++ angular_bootcamp/src/app/employees/employee-details/employee-details.component.ts
@@ -1,18 +1,9 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DatePipe, UpperCasePipe } from '@angular/common';
-import {
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Employee } from '../../models/employee.model';
 import { EmployeeDetailsSkillsComponent } from '../employee-details-skills/employee-details-skills.component';
 import { EmployeeDetailsProjectsComponent } from '../employee-details-projects/employee-details-projects.component';
-import { EmployeesComponent } from '../employees-main/employees.component';
 
 @Component({
   selector: 'app-employee-details',
@@ -28,48 +19,53 @@ import { EmployeesComponent } from '../employees-main/employees.component';
   templateUrl: './employee-details.component.html',
   styleUrl: './employee-details.component.scss',
 })
-export class EmployeeDetailsComponent implements OnInit, OnChanges {
-  @Input() employee?: Employee;
+export class EmployeeDetailsComponent implements OnInit {
+  private _employee!: Employee;
   @Output() updatedEmployee: EventEmitter<Employee> = new EventEmitter<Employee>();
+
   employeeForm!: FormGroup<{
     name: FormControl<string | null>;
     surname: FormControl<string | null>;
-    skillsList: FormArray<any>;
-    projectsList: FormArray<any>;
+    hireDate: FormControl<string | null>;
   }>;
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
     this.employeeForm = this.formBuilder.group({
       name: ['', Validators.required],
       surname: ['', Validators.required],
-      skillsList: this.formBuilder.array([]),
-      projectsList: this.formBuilder.array([]),
+      hireDate: ['', Validators.required],
     });
-
     this.employeeForm.valueChanges.subscribe((value) => {
       const editedEmployee = {
         ...this.employee,
         ...value,
       };
-      console.log('eee');
+      console.log('cos sise stalo');
       this.updatedEmployee.emit(editedEmployee as Employee);
     });
+  }
+
+  ngOnInit(): void {
+    console.log('eee');
   }
 
   getManagerFullName(): string {
     return this.employee?.manager?.name + ' ' + this.employee?.manager?.surname;
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes && this.employee) {
-      this.employeeForm = this.formBuilder.group({
-        name: [this.employee!.name, Validators.required],
-        surname: [this.employee!.surname, Validators.required],
-        skillsList: this.formBuilder.array([this.employee.skillsList.map((skill) => new FormControl(skill))]),
-        projectsList: this.formBuilder.array([]),
+  @Input()
+  public set employee(employee: Employee) {
+    this._employee = employee;
+    if (employee) {
+      this.employeeForm.patchValue({
+        name: employee.name,
+        surname: employee.surname,
+        hireDate: employee.hireDate.toString(),
       });
     }
+  }
+
+  public get employee() {
+    return this._employee;
   }
 }
